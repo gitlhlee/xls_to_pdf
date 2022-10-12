1cff460c26894cbd6d2453bfe9045b170581b590
laehoonlee <xeo@uiscloud.net>
2022-10-12 10:47:00 +0900

    초기 커밋

---
+++ src/test/java/org/gmr/xls_to_pdf/XlsToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gmr.xls_to_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
